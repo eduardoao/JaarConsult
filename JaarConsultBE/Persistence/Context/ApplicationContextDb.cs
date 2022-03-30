bb180cb6dd83c9dbb7f7aeefc655ebf7f63c6e17
Eduardo Alcantara de Oliveira <eoliveira@trt4.jus.br>
2022-03-30 16:36:23 -0300

    Adicionando pacotes para EntityFrameWork Core

---
+++ JaarConsultBE/Persistence/Context/ApplicationContextDb.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public class ApplicationContextDb : DbContext, IAplicationContextDb
+    {
+        public ApplicationContextDb(DbContextOptions<ApplicationContextDb> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Product>().Property(p => p.Rate).HasColumnType("decimal(10, 2)");
+
+            base.OnModelCreating(modelBuilder);
+        }
+        //public DbSet<Product> Products { get; set; }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ JaarConsultBE/Persistence/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interface;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+
+namespace Persistence
+{
+    public static class DependencyInjection
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationContextDb>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationContextDb).Assembly.FullName)));
+
+            services.AddScoped<IAplicationContextDb>(provider => provider.GetService<ApplicationContextDb>());
+        }
+    }
+}
