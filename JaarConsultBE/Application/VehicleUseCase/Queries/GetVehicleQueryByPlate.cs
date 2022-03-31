ab0c539bb19673dbffb05353e488e1bc4ccd35f7
Eduardo Alcantara de Oliveira <eoliveira@trt4.jus.br>
2022-03-31 14:22:52 -0300

    Criacao da classe controller

--- JaarConsultBE/Application/VehicleUseCase/Queries/GetVehicleQueryByPlate.cs
+++ JaarConsultBE/Application/VehicleUseCase/Queries/GetVehicleQueryByPlate.cs
@@ -8,10 +8,10 @@ using System.Threading.Tasks;
 
 namespace Application.VehicleUseCase.Queries
 {
-    public class GetVehicleQueryByCode : IRequest<Veiculo>
+    public class GetVehicleQueryByPlate : IRequest<Veiculo>
     {
-        public string Code { get; set; }
-        public class GetVehicleQueryByCodeHandler : IRequestHandler<GetVehicleQueryByCode, Veiculo>
+        public string Plate { get; set; }
+        public class GetVehicleQueryByCodeHandler : IRequestHandler<GetVehicleQueryByPlate, Veiculo>
         {
             private readonly IAplicationContextDb _context;
 
@@ -20,9 +20,9 @@ namespace Application.VehicleUseCase.Queries
                 _context = context;
             }          
 
-            public async Task<Veiculo> Handle(GetVehicleQueryByCode request, CancellationToken cancellationToken)
+            public async Task<Veiculo> Handle(GetVehicleQueryByPlate request, CancellationToken cancellationToken)
             {
-                var vehicle = _context.Veiculos.Where(a => a.CodigoFipe == request.Code).FirstOrDefault();
+                var vehicle = _context.Veiculos.Where(a => a.Placa == request.Plate).FirstOrDefault();
                 if (vehicle == null) return null;
                 return vehicle;
             }
---
+++ JaarConsultBE/WebApi/Controllers/v1/VehicleController.cs
@@ -0,0 +1,58 @@
+﻿using Application.VehicleUseCase.Commands;
+using Application.VehicleUseCase.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : BaseApiController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Create (CreateVehicleCommand vehicleCommand)
+        {
+            return Ok(await Mediator.Send(vehicleCommand));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update (Guid id,UpdateVehicleCommand vehicleCommand)
+        {
+            if (id != vehicleCommand.Id) return BadRequest();
+            return Ok(await Mediator.Send(vehicleCommand));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete (Guid id)
+        {
+            return Ok(await Mediator.Send(new DeleteVehicleCommand { Id = id }));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllVehiclesQuery()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok(await Mediator.Send(new GetVehicleQueryById { Id = id }));
+        }
+
+        [HttpGet("{plate}")]
+        public async Task<IActionResult> GetByPlate(string plate)
+        {
+            return Ok(await Mediator.Send(new GetVehicleQueryByPlate { Plate = plate }));
+        }
+
+        [HttpGet("{codefipe}")]
+        public async Task<IActionResult> GetByCodeFipe(string codeFipe, int year)
+        {
+            return Ok(await Mediator.Send(new GetVehicleQueryByCodeFipe { Code = codeFipe , Year = year }));
+        }
+    }
+}
