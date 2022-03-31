744f5281ee1f437d67e218be0609920cabb411ad
Eduardo Alcantara de Oliveira <eoliveira@trt4.jus.br>
2022-03-31 16:24:06 -0300

    Inclusao de regra de validacao para inclusao

--- JaarConsultBE/Application/DependencyInjection.cs
+++ JaarConsultBE/Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.PipelineBehaviour;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -9,6 +10,8 @@ namespace Application
         public static void AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         }
     }
 }
---
+++ JaarConsultBE/Application/Validators/CreateVehicleCommandValidators.cs
@@ -0,0 +1,24 @@
+﻿using Application.VehicleUseCase.Commands;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators
+{
+    public class CreateVehicleCommandValidators : AbstractValidator<CreateVehicleCommand>
+    {
+        public CreateVehicleCommandValidators()
+        {           
+            RuleFor(v => v.Valor).NotEmpty();
+            RuleFor(v => v.AnoModelo).NotEmpty();
+            RuleFor(v => v.CodigoFipe).NotEmpty();
+            RuleFor(v => v.Combustivel).NotEmpty();          
+            RuleFor(v => v.Marca).NotEmpty();
+            RuleFor(v => v.Placa).NotEmpty();
+            RuleFor(v => v.MesReferencia).NotEmpty();
+        }
+    }
+}
